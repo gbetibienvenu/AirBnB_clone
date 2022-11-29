65bf37b5fd2b81e1167789f89dfe22ef7c948f57
Technicalqit <collinsomanudhowo2@gmail.com>
2022-11-29 10:30:09 -0800

    Console

--- console.py
+++ console.py
@@ -3,7 +3,6 @@
 This file defines the console class which will
 serve as the entry point of the entire project
 """
-
 from cmd import Cmd
 from models import storage
 from models.engine.errors import *
